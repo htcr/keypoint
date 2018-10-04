90735ac65cc0135860017aa7dfb9650cdb3690a5
htcr <congruihetang@gmail.com>
2018-10-04 02:54:35 -0400

    finished panoramas

--- panoramas.py
+++ panoramas.py
@@ -18,7 +18,7 @@ def imageStitching(im1, im2, H2to1):
     '''
     H, W = im1.shape[0:2]
     tH, tW = 2*H, 2*W
-    translate_M = np.array([[1, 0, 0], [0, 1, H/2.0], [0, 0, 1]], dtype=np.float32)
+    translate_M = np.array([[1, 0, 0], [0, 1, 0*H/2.0], [0, 0, 1]], dtype=np.float32)
     pano_im2 = cv2.warpPerspective(im2, translate_M @ H2to1, (tW, tH))
     pano_im1 = cv2.warpPerspective(im1, translate_M, (tW, tH))
     pano_im = np.maximum(pano_im1, pano_im2)
@@ -33,8 +33,34 @@ def imageStitching_noClip(im1, im2, H2to1):
     ''' 
     ######################################
     # TO DO ...
+    H1, W1 = im1.shape[0:2]
+    im1_corners = np.array([[0, 0], [0, H1], [W1, 0], [W1, H1]], dtype=np.float32).transpose((1, 0))
+    H2, W2 = im2.shape[0:2]
+    im2_corners_homo = np.array([[0, 0, 1], [0, H2, 1], [W2, 0, 1], [W2, H2, 1]], dtype=np.float32).transpose((1, 0))
+    im2_corners = H2to1 @ im2_corners_homo
+    im2_corners[0, :] /= im2_corners[2, :]
+    im2_corners[1, :] /= im2_corners[2, :]
+    im2_corners = im2_corners[0:2, :]
+    corners = np.concatenate((im1_corners, im2_corners), axis=1)
+    lt = np.min(corners, axis=1)
+    rb = np.max(corners, axis=1)
+    tW, tH = rb - lt
+    
+    translate_M = np.array([[1, 0, -lt[0]], [0, 1, -lt[1]], [0, 0, 1]], dtype=np.float32)
+    pano_im2 = cv2.warpPerspective(im2, translate_M @ H2to1, (int(tW), int(tH)))
+    pano_im1 = cv2.warpPerspective(im1, translate_M, (int(tW), int(tH)))
+    pano_im = np.maximum(pano_im1, pano_im2)
+
     return pano_im
 
+
+def generatePanorama(im1, im2):
+    locs1, desc1 = briefLite(im1)
+    locs2, desc2 = briefLite(im2)
+    matches = briefMatch(desc1, desc2)
+    H2to1 = ransacH(matches, locs1, locs2, num_iter=5000, tol=2)
+    pano_im_no_clip = imageStitching_noClip(im1, im2, H2to1)
+    return pano_im_no_clip
 
 if __name__ == '__main__':
     im1 = cv2.imread('../data/incline_L.png')
@@ -45,9 +71,16 @@ if __name__ == '__main__':
     matches = briefMatch(desc1, desc2)
     # plotMatches(im1,im2,matches,locs1,locs2)
     H2to1 = ransacH(matches, locs1, locs2, num_iter=5000, tol=2)
-    pano_im = imageStitching(im1, im2, H2to1)
+    # pano_im = imageStitching(im1, im2, H2to1)
     print(H2to1)
-    cv2.imwrite('../results/panoImg.png', pano_im)
-    cv2.imshow('panoramas', pano_im)
+    # np.save('../results/q6_1.npy', H2to1)
+    # cv2.imwrite('../results/6_1.jpg', pano_im)
+    # cv2.imshow('panoramas', pano_im)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    # pano_im_no_clip = imageStitching_noClip(im1, im2, H2to1)
+    pano_im_no_clip = generatePanorama(im1, im2)
+    cv2.imwrite('../results/q6_2_pan.jpg', pano_im_no_clip)
+    cv2.imshow('panoramas_no_clip', pano_im_no_clip)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
